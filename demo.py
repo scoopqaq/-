450cc812af05b9ed18eb8ca05734f9f76a595a6f
scoop <2052235934@qq.com>
2024-08-14 11:28:01 +0800

    1.0

---
+++ demo.py
@@ -0,0 +1,14 @@
+'''
+Author: Li_Hai_Long
+Date: 2024-08-14 11:05:51
+LastEditors: scoop 2052235934@qq.com
+LastEditTime: 2024-08-14 11:17:11
+FilePath: \渲染时间预测\demo.py
+Description: 
+
+'''
+print(1+1)
+a=1+1
+#那就有点可惜了啊
+b=1
+c=2
